f2b5b4528bbd9544d6ea481793e6ce0325236261
siyadkv <siyadkv1000@gmail.com>
2024-05-17 15:43:13 +0400

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,6 +8,7 @@ sap.ui.define(
       return BaseController.extend("project1.controller.App", {
         onInit: function() {
            var gt = "https://goof=gle.com";
+           var test =1; 
         }
       });
     }
